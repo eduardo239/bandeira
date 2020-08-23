3060156b9b25e8d702af3d02c06ba030b265deb7
eduardo239 <eucrieiumaconta@gmail.com>
2020-08-23 20:58:23 -0300

    fix

--- src/bandeiras/Bandeiras.java
+++ src/bandeiras/Bandeiras.java
@@ -1,10 +1,14 @@
 package bandeiras;
 
+import javafx.scene.image.Image;
+
+import java.awt.Toolkit;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.dao.BandeiraDAO;
 
 /**
  * @author eucri
@@ -16,8 +20,14 @@ public class Bandeiras extends Application {
         
         Parent root = FXMLLoader.load(getClass().getResource("UI.fxml"));
         Scene scene = new Scene(root);
+        
+        stage.getIcons().add(new Image("/resources/a.png"));
+        
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
+        
+        
     }
 
     /**
@@ -25,8 +35,11 @@ public class Bandeiras extends Application {
      */
     public static void main(String[] args) {
         launch(args);
+        BandeiraDAO dao = new BandeiraDAO();
+            dao.createTable();
         
         
     }
+    
     
 }
